2ed6a9c12331a5c3dc7520a5cbac626d4fd8f93b
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-08-11 15:58:36 +0200

    Added new action and view.

--- HotDogsApplication/Resources/Resource.Designer.cs
+++ HotDogsApplication/Resources/Resource.Designer.cs
@@ -57,11 +57,39 @@ namespace HotDogsApplication
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050003
+			public const int descriptionTextView = 2131034115;
+			
+			// aapt resource value: 0x7f050000
+			public const int hotDogImageView = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int hotDogNameTextView = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int shortDescriptionTextView = 2131034114;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int HotDogDetailView = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
