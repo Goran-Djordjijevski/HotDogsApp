392410161ae7cd01f84e4fd9bb6873853cf64b84
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-08-11 17:03:56 +0200

    Added new view and action.

--- HotDogsApplication/HotDogDetailActivity.cs
+++ HotDogsApplication/HotDogDetailActivity.cs
@@ -9,17 +9,48 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using HotDogsApp.Model;
+using HotDogsApp.Service;
 
 namespace HotDogsApplication
 {
     [Activity(Label = "Hot Dog Detail", MainLauncher = true)]
     public class HotDogDetailActivity : Activity
     {
+        private ImageView hotDogImageView;
+        private TextView hotDogNameTextView;
+        private TextView shortDescriptionTextView;
+        private TextView descriptionTextView;
+        private TextView priceTextView;
+        private EditText amountEditText;
+        private Button cancelButton;
+        private Button orderButton;
+
+        private HotDog selectedHotDog;
+        private HotDogDataService dataService;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
+            SetContentView(layoutResID: Resource.Layout.HotDogDetailView);
+
+            HotDogDataService dataService = new HotDogDataService();
+            selectedHotDog = dataService.GetHotDogById(hotDogId: 1);
+
             // Create your application here
+        }
+
+        private void FindViews()
+        {
+            hotDogImageView = FindViewById<ImageView>(Resource.Id.hotDogImageView);
+            hotDogNameTextView = FindViewById<TextView>(Resource.Id.hotDogNameTextView);
+            shortDescriptionTextView = FindViewById<TextView>(Resource.Id.shortDescriptionTextView);
+            descriptionTextView = FindViewById<TextView>(Resource.Id.descriptionTextView);
+            priceTextView = FindViewById<TextView>(Resource.Id.priceTextView);
+            amountEditText = FindViewById<EditText>(Resource.Id.amountEditText);
+            cancelButton = FindViewById<Button>(Resource.Id.cancelButton);
+            orderButton = FindViewById<Button>(Resource.Id.orderButton);
         }
     }
 }
